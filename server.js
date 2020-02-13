33aa565d0a6c0feb1b43c546a3dfe740ab3cea47
Jace <jacechan.dev@gmail.com>
2020-02-13 20:48:16 +0800

    Branch update

--- server.js
+++ server.js
@@ -7,16 +7,18 @@ const app = express();
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/', function(req, res) {
-  var weight = parseFloat(req.body.weight);
-  var height = parseFloat(req.body.height);
-  var bmi = weight / (height**2);
-  res.send("Your BMI is " + bmi);
+var weight = parseFloat(req.body.weight);
+var height = parseFloat(req.body.height);
+var bmi = weight / (height**2);
+res.send("Your BMI is " + bmi);
 });
 
 app.listen(3000, function() {
-  console.log("Server is running on port 3000");
+console.log("Server is running on port 3000");
 });
+
+// Added a new comment
